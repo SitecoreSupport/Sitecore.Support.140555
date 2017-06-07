70d3e12d68d8a7f7ef447609ebf195cd109eb3f0
DK\ili <ili@sitecore.net>
2017-06-07 13:58:20 +0300

    no message

---
+++ src/Sitecore.Support.140555/Save.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines.Save;
+
+namespace Sitecore.Support.Pipelines.Save
+{
+    public class Save : Sitecore.Pipelines.Save.Save
+    {
+        public new void Process(SaveArgs args)
+        {
+            foreach (SaveArgs.SaveItem item in args.Items)
+            {
+                foreach (SaveArgs.SaveField field in item.Fields)
+                {
+                    if (field.Value == "<br>")
+                    {
+                        field.Value = String.Empty;
+                    }
+                }
+            }
+            base.Process(args);
+        }
+    }
+}
